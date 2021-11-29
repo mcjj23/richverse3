6024ee5268261d43afd74b6d4d9f6400245045b5
mcjj23 <hongyujieiris@gmail.com>
2021-11-29 15:31:04 +0800

    web-mobile-src

---
+++ src/settings.0fcd8.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/scene - 001.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"468ae",resources:"216c0",main:"31f98"}};
